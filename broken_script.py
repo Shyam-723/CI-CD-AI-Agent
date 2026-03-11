c7e3029e2c411d3fecc847c621d79c2b03ec763a
Shyam Gupta <shyam.gupta0723@gmail.com>
2026-03-11 10:23:33 -0700

    Add tracked python script for testing

---
+++ broken_script.py
@@ -0,0 +1,7 @@
+def main():
+    print("This script is running...")
+    # This will fail with a NameError because 'undefined_variable' is not defined
+    print(undefined_variable)
+
+if __name__ == "__main__":
+    main()
